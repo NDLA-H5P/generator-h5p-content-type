1c14184ff12fc57bc4bdd909d736f4061e56a3a9
Sindre Bøyum <sindre.boyum@gmail.com>
2021-10-06 10:07:59 +0200

    functionify App component

--- src/ts-react/templates/root/src/App.tsx
+++ src/ts-react/templates/root/src/App.tsx
@@ -1,19 +1,16 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
 
-type Props = {
+export type AppProps = {
   adjective: string;
-}
+};
 
-class App extends React.Component<Props> {
-  render() {
-    const { adjective } = this.props;
-    return (
-      <>
-        <h1>Hi, you're {adjective}</h1>
-      </>
-    );
-  }
+const App: React.FC<AppProps> = ({ adjective }) => {
+  return (
+    <>
+      <h1>Hi, you're {adjective}</h1>
+    </>
+  );
 }
 
 export default hot(App);
